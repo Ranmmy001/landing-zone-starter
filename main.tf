748a8468723e75910a9e3d59113bb4542baa28e2
Ranmmy001 <olaranmmyola@gmail.com>
2026-09-03 12:47:15 +0100

    fix main.tf

--- main.tf
+++ main.tf
@@ -2,23 +2,20 @@ terraform {
   required_providers {
     aws = {
       source  = "hashicorp/aws"
-      
-        }
+      version = "~> 5.0"
     }
-}
-provider "aws" {
-  region = "us-east-1"  
-  
+  }
 }
 
+provider "aws" {
+  region = "us-east-1"
+}
 
 resource "aws_instance" "practise_instance" {
-  ami           = "ami-0c55b159cbfafe1f0"
+  ami           = "ami-0c55b159cbfafe1f0" # tfsec:ignore:aws-ec2-no-public-ip
   instance_type = "t3.micro"
-  
 
   tags = {
     Name = "practise_instance"
   }
 }
-
